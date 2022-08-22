6cf2d99fc6891cf996c25efc7cb17f89ac4f4705
call121233 <call121233@naver.com>
2022-08-22 10:23:14 +0900

    20220822 10:23

---
+++ Ch02 개발환경구성/ch02.js
@@ -0,0 +1,23 @@
+/**
+ * 2.1
+ * 비주얼 스튜디오 코드(Visual Studio Code) : https://code.visualstudio.com
+ * 
+ * 2.2
+ * Node.js(자바스크립트 런타임 환경) 설치
+ * 라이브러리(패키지) 설치 및 적용 : https://nodejs.org
+ * 
+ * node -v
+ * npm -v
+ * 
+ * 2.3 
+ * 자바스크립트 개발을 위한 vscode Extension 설치
+ * JavaScript (ES6) code snippets
+ * HTML Snippets
+ * ESLint(문법 오류 체크)
+ * Prettier - Code formatter
+ *    File > Preference > Settings
+ *    검색창에 'formatter' 입력 
+ *    Editor:Default Formatter 설정
+ *    Format On Save 선택
+ * Live Server
+ */
